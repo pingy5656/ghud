373d91b444e7e822421aa2e0b55c7608193378dc
pingy5656 <pingy5656mc@gmail.com>
2023-09-04 18:14:57 -0700

    Update sh_load.lua

--- lua/darkrp_modules/glorioushud/sh_load.lua
+++ lua/darkrp_modules/glorioushud/sh_load.lua
@@ -1,20 +1,18 @@
-local files = file.Find( 'darkrp_modules/glorioushud/locales/*.lua', 'LUA' )
+local files = file.Find('darkrp_modules/glorioushud/locales/*.lua', 'LUA')
 
-for i, v in pairs( files ) do
-
-	if SERVER then
-		AddCSLuaFile( 'darkrp_modules/glorioushud/locales/' .. v )
-	else
-		include( 'darkrp_modules/glorioushud/locales/' .. v )
-	end
-
+for i, v in pairs(files) do
+    if SERVER then
+        AddCSLuaFile('darkrp_modules/glorioushud/locales/' .. v)
+    else
+        include('darkrp_modules/glorioushud/locales/' .. v)
+    end
 end
 
 -- Adding the magic system files
 if SERVER then
-	AddCSLuaFile( 'darkrp_modules/glorioushud/cl_magichud.lua' )
-	AddCSLuaFile( 'darkrp_modules/glorioushud/cl_magicinfo.lua' )
+    AddCSLuaFile('darkrp_modules/glorioushud/cl_magic_hud.lua')
+    AddCSLuaFile('darkrp_modules/glorioushud/cl_magicinfo.lua')
 else
-	include( 'darkrp_modules/glorioushud/cl_magichud.lua' )
-	include( 'darkrp_modules/glorioushud/cl_magicinfo.lua' )
+    include('darkrp_modules/glorioushud/cl_magic_hud.lua')
+    include('darkrp_modules/glorioushud/cl_magicinfo.lua')
 end
